8da27c96d504a9c02d03e94ee55ef2392834395d
Chris Roche <chris@wetrack.com>
2019-09-29 15:59:08 +0100

    Delegates

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+
+    public class PhotoProcessor
+    {
+        // public delegate void PhotoFilterHandler(Photo photo); Custom delegate
+
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+
+
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+
+            var photoFilters = new PhotoFilters();
+
+            //PhotoProcessor.PhotoFilterHandler filterHandler = photoFilters.ApplyBrightness; Custom delegate
+            Action<Photo> filterHandler = photoFilters.ApplyBrightness;
+
+
+            filterHandler += photoFilters.Resize;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("Test path", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            System.Console.WriteLine("Remove redeye");
+        }
+
+    }
+
+}
