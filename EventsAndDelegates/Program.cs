b6dce2fdcc76b21397a11fae3192b241440107ca
Chris Roche <chris@wetrack.com>
2019-10-01 22:09:19 +0100

    Events & Delegates

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "My video" };
+            var videoEncoder = new VideoEncoder(); // Publisher
+            var mailservice = new MailService(); // Subscriber
+            var messageService = new MessageService(); // Subscriber
+
+            videoEncoder.VideoEncoded += mailservice.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+            
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1 - Define a Delegate
+        // 2 - Define an event based on that delegate
+        // 3 - Raise the event
+
+        //public delegate void VideoEncoderEventHandler(object source, VideoEventArgs args);  // 1
+        //public event VideoEncoderEventHandler VideoEncoded; // 2
+
+        public event EventHandler<VideoEventArgs> VideoEncoded; // 1 & 2
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        // Event publishing methods should be virtual & protected
+        protected virtual void OnVideoEncoded(Video video) // 3
+        {
+            VideoEncoded?.Invoke(this, new VideoEventArgs() { Video = video });
+        }
+
+    }
+}
