2efa69ed654107877b182fe18a1b001b62acd02e
Chris Roche <chris@wetrack.com>
2019-10-11 17:12:45 +0100

    Extension methods

---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords is < 0");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(" ");
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords));
+
+        }
+    }
+
+
+}
